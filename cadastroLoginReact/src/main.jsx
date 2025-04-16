e8e5bbf5eda83cb81ab2ec9753e3533fe86a3d2a
Murillo-lc <murillo.marques@sptech.school>
2025-04-16 20:15:16 -0300

    bruna pediu pra ver pr

--- cadastroLoginReact/src/main.jsx
+++ cadastroLoginReact/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import Cadastro from './Cadastro'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Cadastro />
-  </StrictMode>,
+  </StrictMode>
 )
